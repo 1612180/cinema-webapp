8659cc49c4f31c155e1176f4f26a548b504afaad
1612180 <ngtranhau@gmail.com>
2019-05-09 16:11:05 +0700

    Them trang chi tiet phim Small fix to header and footer (match color)

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const port = process.env.PORT || 9090
 
 // module routes
 const indexRoute = require('./routes/index')
+const chitietphimRoute = require('./routes/chitietphim')
 
 // serve images, CSS files, and JavaScript files
 app.use(express.static('public'))
 
 app.use('/', indexRoute)
+
+app.use('/chitietphim', chitietphimRoute)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
