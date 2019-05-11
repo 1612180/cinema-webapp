7176ff5f97054e450df533d9aaeeb764f7bf7154
1612180 <ngtranhau@gmail.com>
2019-05-11 14:10:49 +0700

    Them trang gio hang

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 9090
 // module routes
 const indexRoute = require('./routes/index')
 const chitietphimRoute = require('./routes/chitietphim')
+const giohangRoute = require('./routes/giohang')
 
 // serve images, CSS files, and JavaScript files
 app.use(express.static('public'))
@@ -12,6 +13,8 @@ app.use(express.static('public'))
 app.use('/', indexRoute)
 
 app.use('/chitietphim', chitietphimRoute)
+
+app.use('/giohang', giohangRoute)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
