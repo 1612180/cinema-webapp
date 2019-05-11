519d3d79347c9ab9055d403f8bdd931acd8aa734
1612180 <ngtranhau@gmail.com>
2019-05-11 16:33:13 +0700

    Them lich su don hang Chinh sua thong tin bang chi tiet phim Increase font cover weight

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 9090
 const indexRoute = require('./routes/index')
 const chitietphimRoute = require('./routes/chitietphim')
 const giohangRoute = require('./routes/giohang')
+const lichsudonhangRoute = require('./routes/lichsudonhang')
 
 // serve images, CSS files, and JavaScript files
 app.use(express.static('public'))
@@ -15,6 +16,8 @@ app.use('/', indexRoute)
 app.use('/chitietphim', chitietphimRoute)
 
 app.use('/giohang', giohangRoute)
+
+app.use('/lichsudonhang', lichsudonhangRoute)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
---
+++ routes/lichsudonhang.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+// '../' is malicous
+const path = require('path')
+
+router.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/../views/lichsudonhang.html'))
+})
+
+module.exports = router
